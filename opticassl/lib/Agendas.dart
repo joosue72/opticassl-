a4e1f808e247a6de4c92287f533b9965e0a198bd
Josué <joosue72@hotmail.com>
2020-08-02 23:03:25 -0500

    Version 0.5

--- opticassl/lib/Agendas.dart
+++ opticassl/lib/Agendas.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import 'package:opticassl/Menu.dart';
-
+import 'package:numberpicker/numberpicker.dart';
 
 
 
@@ -17,9 +18,31 @@ import 'package:opticassl/Menu.dart';
 
 TextEditingController _textCosto = TextEditingController();
 String id;
+String _dateTime;
 final db = Firestore.instance;
+TimeOfDay _time = TimeOfDay.now();
+TimeOfDay picked;
+int _currentValue = 1;
+String hora;
+String formattedDate;
 
 class _AgendasState extends State<Agendas> {
+
+  Future<Null> selectTime(BuildContext context) async{
+    picked = await showTimePicker(
+      context: context, 
+      initialTime: _time,
+      
+      );
+
+      
+
+    setState(() {
+      hora =_time.format(context);
+      _time = picked;
+      print(_time);
+    });
+  }
 
 
 TextFormField buildTextFormFieldCosto() {
@@ -46,20 +69,114 @@ TextFormField buildTextFormFieldCosto() {
   }
 
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.black,
+       backgroundColor: Colors.white,
       appBar: _getCustomAppBar(), 
       body: ListView(
         padding: EdgeInsets.all(8),
+        
         children: <Widget>[
-
+         
           SizedBox(height: 50.0),
           Form(
             child: buildTextFormFieldCosto(),
+            
+            
           ),
-           SizedBox(height: 50.0),
+
+          
+         
+           SizedBox(height: 20.0),
+           
+           SizedBox(height: 20.0),
+           Form(
+
+                      child: new Container(
+              child: new Row(
+
+                children: <Widget>[
+
+                Text(_dateTime == null ? 'No hay fecha seleccionada' : _dateTime.toString(),textAlign: TextAlign.center,style: TextStyle(fontSize: 25),),
+
+
+                new IconButton(
+
+                  
+                  color: Color(0xFF009688), 
+                   icon: Icon(Icons.calendar_today,size: 30, ),
+                          onPressed: () {
+                               showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2001), lastDate: DateTime(2222)).then((date){
+                                   formattedDate = DateFormat('dd-MM-yyyy').format(date);
+                      setState(() {
+                        print(formattedDate);
+                        _dateTime = formattedDate;
+                      });
+                  });
+                              
+                          },
+                 
+
+
+                  ),
+
+
+                ],
+              ),
+
+              
+            ),
+                    
+              
+           ),
+             SizedBox(height: 50.0),
+          Form(
+          
+            
+                      child: new Container(
+              child: new Row(
+
+                children: <Widget>[
+
+                Text( _time.format(context),textAlign: TextAlign.center,style: TextStyle(fontSize: 25),),
+
+
+                IconButton(
+                   color: Color(0xFF009688), 
+              icon: Icon(Icons.alarm,size: 30, ),
+              onPressed: (){
+                selectTime(context);
+              
+              }
+            ,),
+
+
+                ],
+              ),
+
+              
+            ),
+                   
+          ),
+            SizedBox(height: 10.0,),
+          Form(
+            child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new NumberPicker.integer(
+                
+                initialValue: _currentValue,
+                minValue: 1,
+                maxValue: 10,
+                onChanged: (newValue) =>
+                    setState(() => _currentValue = newValue)),
+            new Text("Sucursal: $_currentValue",style: TextStyle(color: Colors.white),),
+          ],
+        ),
+          ),
+          SizedBox(height: 70.0),
           ButtonTheme(
                 
                 minWidth: 250.0,
@@ -101,8 +218,12 @@ TextFormField buildTextFormFieldCosto() {
 
   void createData() async {
   String nombre;
+  String fecha;
   nombre =_textCosto.text;
-        DocumentReference ref = await db.collection('Agendas').add({'Nombre': '$nombre'});
+  hora =_time.format(context);
+  fecha = formattedDate;
+      print(hora);
+        DocumentReference ref = await db.collection('Agendas').add({'Fecha': '$fecha','Nombre':'$nombre','Hora':'$hora','Sucursal':'$_currentValue'});
       setState(() => id = ref.documentID);  
   }
 
